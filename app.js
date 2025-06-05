b5c69428dbeb4c3007db3987c3e1ed9c26d533aa
sayel <mdsayel111@gmail.com>
2025-06-05 17:45:19 +0600

    complete

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // set window width and height to variables for daynamic styling
+const prevWindowWidth = document.documentElement.clientWidth;
 
 const setWindowWidthAndHeight = () => {
   // const windowWidth = document.documentElement.clientWidth;
   const windowWidth = document.documentElement.clientWidth;
-  console.log(windowWidth);
-
   const windowHeight = document.documentElement.clientHeight;
+
+  // if (prevWindowWidth !== windowWidth) {
+  //   return;
+  // }
   document.documentElement.style.setProperty(
     "--window-width",
     windowWidth / 100
@@ -15,6 +18,7 @@ const setWindowWidthAndHeight = () => {
     windowHeight / 100
   );
 };
+
 setWindowWidthAndHeight();
 
 window.addEventListener("resize", setWindowWidthAndHeight);
