400be6f63d4214ae2748b26940db9d43e0561560
sayel <mdsayel111@gmail.com>
2025-06-05 18:56:20 +0600

    complete task

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const setWindowWidthAndHeight = () => {
   // const windowWidth = document.documentElement.clientWidth;
   const windowWidth = document.documentElement.clientWidth;
   const windowHeight = document.documentElement.clientHeight;
-
-  // if (prevWindowWidth !== windowWidth) {
-  //   return;
-  // }
   document.documentElement.style.setProperty(
     "--window-width",
     windowWidth / 100
